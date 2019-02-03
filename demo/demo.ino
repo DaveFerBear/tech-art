62cda029484d235a4935798929bb8b6ffff00639
David Ferris <david.j.ferris.21@gmail.com>
2019-02-03 18:32:53 -0500

    Working motor control with relays.

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,20 +1,40 @@
 int ledPin = 13;
-int motorPin = 12;
 int IRSensorPin = A0;
+
+// Motor, Relay
+int m1_r1 = 4;
+int m1_r2 = 5;
+int m2_r1 = 6;
+int m2_r2 = 7;
+
+// Motor Controller
+int m1 = 22;
+int m2 = 24;
 
 int cnt;
 
 void setup()
 {
   Serial.begin(9600);
-  
+
+  // Sensors
   pinMode(ledPin, OUTPUT);
   pinMode(IRSensorPin, INPUT);
-  
+
+  // Relays
+  pinMode(m1_r1, OUTPUT);
+  pinMode(m1_r2, OUTPUT);
+  pinMode(m2_r1, OUTPUT);
+  pinMode(m2_r2, OUTPUT);
+
+  // Motors
+  pinMode(m1, OUTPUT);
+  pinMode(m2, OUTPUT);
+
   cnt = 0;
 }
 
-void rampMotor() {
+void rampMotor(int motorPin) {
   delay(50);
   cnt += 1;
   cnt = cnt%255;
@@ -24,12 +44,36 @@ void rampMotor() {
 
 int getIRDistance(int pin) {
   // Mapping function: http://mess.uwaterloo.ca/datasheet/481D.pdf
-  int cm = 12.0 / (float(analogRead(pin)) / 1023.0 * 5.0);
+  int cm = 14.0 / (float(analogRead(pin))/1023.0*5.0);
   if (cm > 150) cm=150;
+  
+  // Alternate datasheet: http://mess.uwaterloo.ca/datasheet/480D.pdf
+  // int cm = 22.33/((float(analogRead(pin))/1023.0*5.0) - 0.189) - 0.42;
   return cm;
+}
+
+void set_motor_direction(int motor, bool dir) {
+  if (motor == 1) {
+    digitalWrite(m1_r1, dir);
+    digitalWrite(m1_r2, dir);
+  } else if (motor == 2) {
+    digitalWrite(m2_r1, dir);
+    digitalWrite(m2_r2, dir);
+  }
 }
 
 void loop()
 {
-  Serial.println(getIRDistance(IRSensorPin));
+  set_motor_direction(1, true);
+  set_motor_direction(2, true);
+  digitalWrite(m1, HIGH);
+  digitalWrite(m2, HIGH);
+  delay(1000);
+
+  set_motor_direction(1, false);
+  set_motor_direction(2, false);
+  digitalWrite(m1, HIGH);
+  digitalWrite(m2, HIGH);
+  delay(1000);
 }
+
