a2760f07c00e2bdfb04a2ad815241f35598fabaa
David Ferris <david.j.ferris.21@gmail.com>
2019-02-03 14:48:18 -0500

    signal readings working

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 13;
+int motorPin = 12;
+int IRSensorPin = A0;
 
+int cnt;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(IRSensorPin, INPUT);
+  
+  cnt = 0;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void rampMotor() {
+  delay(50);
+  cnt += 1;
+  cnt = cnt%255;
+  analogWrite(ledPin, cnt);
+  analogWrite(motorPin, cnt);
+}
 
+int getIRDistance(int pin) {
+  // Mapping function: http://mess.uwaterloo.ca/datasheet/481D.pdf
+  int cm = 12.0 / (float(analogRead(pin)) / 1023.0 * 5.0);
+  if (cm > 150) cm=150;
+  return cm;
+}
+
+void loop()
+{
+  Serial.println(getIRDistance(IRSensorPin));
 }
