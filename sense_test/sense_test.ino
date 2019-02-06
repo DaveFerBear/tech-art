dcc21a72e746c2db73da457a5eced835f3385c3b
David Ferris <david.j.ferris.21@gmail.com>
2019-02-05 20:25:15 -0500

    Adding extra .ino file for testing Grove PIR sense modules.

---
+++ sense_test/sense_test.ino
@@ -0,0 +1,11 @@
+int sense = A5;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sense, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(sense));
+}
