f0048ea341237675756f8f999247b0e6d7881fa5
David Ferris <david.j.ferris.21@gmail.com>
2019-02-05 20:24:44 -0500

    Motor speed handling

--- demo/demo.ino
+++ demo/demo.ino
@@ -2,14 +2,14 @@ int ledPin = 13;
 int IRSensorPin = A0;
 
 // Motor, Relay
-int m1_r1 = 4;
-int m1_r2 = 5;
-int m2_r1 = 6;
-int m2_r2 = 7;
+int m1_r1 = 22;
+int m1_r2 = 23;
+int m2_r1 = 24;
+int m2_r2 = 25;
 
 // Motor Controller
-int m1 = 22;
-int m2 = 24;
+int m1 = A0;
+int m2 = A1;
 
 int cnt;
 
@@ -52,28 +52,63 @@ int getIRDistance(int pin) {
   return cm;
 }
 
-void set_motor_direction(int motor, bool dir) {
+private void set_motor_direction(int motor, bool forward) {
   if (motor == 1) {
-    digitalWrite(m1_r1, dir);
-    digitalWrite(m1_r2, dir);
+    digitalWrite(m1_r1, forward);
+    digitalWrite(m1_r2, forward);
   } else if (motor == 2) {
-    digitalWrite(m2_r1, dir);
-    digitalWrite(m2_r2, dir);
+    digitalWrite(m2_r1, forward);
+    digitalWrite(m2_r2, forward);
   }
+}
+
+private void set_motor_speed(int motor, int speed) {
+  // speed within [-255,255]
+  int reverse = false;
+  if (speed < 0) {
+    reverse = true;
+  }
+
+  set_motor_direction(motor, reverse);
+  if (motor == 1) {
+    analogWrite(m1, speed);
+  } else if (motor == 2) {
+    analogWrite(m2, speed);
+  }
+}
+
+void cyle_motors() {
+  set_motor_direction(1, true);
+  set_motor_direction(2, true);
+  analogWrite(m1, 1023);
+  analogWrite(m2, 1023);
+  delay(500);
+  analogWrite(m1, 0);
+  analogWrite(m2, 0);
+  
+  delay(2000);
+
+  set_motor_direction(1, false);
+  set_motor_direction(2, false);
+  analogWrite(m1, 1023);
+  analogWrite(m2, 1023);
+  delay(500);
+  analogWrite(m1, 0);
+  analogWrite(m2, 0);
+
+  delay(2000);  
 }
 
 void loop()
 {
   set_motor_direction(1, true);
   set_motor_direction(2, true);
-  digitalWrite(m1, HIGH);
-  digitalWrite(m2, HIGH);
-  delay(1000);
-
-  set_motor_direction(1, false);
-  set_motor_direction(2, false);
-  digitalWrite(m1, HIGH);
-  digitalWrite(m2, HIGH);
-  delay(1000);
+  
+  analogWrite(m1, 1023);
+  analogWrite(m2, 1023);
+  delay(200);
+  
+  analogWrite(m1, 0);
+  analogWrite(m2, 0);
 }
 
